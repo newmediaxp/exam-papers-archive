cedeef8f985254ba5ec76756869a7b832a339983
AnimaxNeil <animaxnei@gmail.com>
2023-05-17 18:33:33 +0530

    added pwa support

--- assets/js/base-script.js
+++ assets/js/base-script.js
@@ -1,7 +1,4 @@
 // regester service script worker for PWA
 
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("epa-sw.js")
-        .then(registration => console.log(registration))
-        .catch(error => console.error(error))
-}
+if ("serviceWorker" in navigator)
+    navigator.serviceWorker.register("epa-sw.js");
