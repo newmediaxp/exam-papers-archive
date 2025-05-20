60a434d996caa683862173b34019583164d28fbd
AnimaxNeil <animaxneil@gmail.com>
2025-05-20 16:56:53 +0530

    ...

--- pug/generate.bat
+++ pug/generate.bat
@@ -1,8 +1,7 @@
-@echo off
-
+@echo build started ...
 call pug ./pug/index.pug -o ./ -P 
 call pug ./pug/about.pug -o ./ -P 
 call pug ./pug/wbsu-ug.pug -o ./ -P 
 call pug ./pug/404.pug -o ./ -P 
-
+@echo ... build finished
 exit
--- pug/generate.sh
+++ pug/generate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+echo build started ...
 pug ./pug/index.pug -o ./ -P
 pug ./pug/about.pug -o ./ -P
 pug ./pug/wbsu-ug.pug -o ./ -P
 pug ./pug/404.pug -o ./ -P
+echo ... build finished
